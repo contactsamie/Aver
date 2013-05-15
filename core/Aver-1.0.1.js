fd717ab76e8c3d1de329ba26f3cff6768690be97
Sam <contactsamie@gmail.com>
2013-05-15 12:32:59 -0400

    Aver
    Updates core

---
+++ core/Aver-1.0.1.js
@@ -0,0 +1,37 @@
+(function (g) {
+    var Output = {};
+    var thisApplication, returnCalls = {};
+    var pActualValue, pExpectedValue, pCondition, pReasonForFailingTest, pStatus, pNameOfTest = {};
+    var pOtherwiseFailBecause = function (reasonForFailingTest) {
+        pReasonForFailingTest = reasonForFailingTest;
+        Output(pStatus, pNameOfTest, pExpectedValue, pActualValue, pReasonForFailingTest);
+    };
+    var ensure = {
+        ToMakeSure: function (actualValue) {
+            pActualValue = actualValue;
+            return returnCalls;
+        }
+    };
+    var pTestFrame = function (pMethod) {
+        pStatus = pMethod(pActualValue, pExpectedValue);
+        return {
+            OtherwiseFailBecause: pOtherwiseFailBecause
+        };
+    };
+    g.Aver = {
+        SetDisplay: function (d) {
+            Output = d;
+        },
+        Plugin: function (plg) {
+            returnCalls[plg.name] = function (expectedValue) {
+                pExpectedValue = expectedValue;
+                return pTestFrame(plg.method);
+            };
+        },
+        WhenTesting: function (nameofTest) {
+            thisApplication = this;
+            pNameOfTest = nameofTest;
+            return ensure;
+        }
+    };
+})(window);
