7fc02c5904700fa84610f02349487cd90a803aa9
Sam <contactsamie@gmail.com>
2013-05-16 08:40:54 -0400

    Avert
    Updated Filenames and removed useless files

--- Display/deployments/AverDisplayBLog-1.0.0.js
+++
@@ -1,9 +0,0 @@
-(function (a) {
-    a.SetDisplay(function (pstatus, pnameofTest, pexpectedValue, pactualValue, preasonForFailingTest) {
-        if (pstatus === true) {
-            console.log(pnameofTest + " Passed!");
-        } else {
-            console.log(pnameofTest + " Failed! - Result Expected: " + pexpectedValue + ", Result Getting : " + pactualValue + " | '" + preasonForFailingTest + "'");
-        }
-    });
-})(Aver);
